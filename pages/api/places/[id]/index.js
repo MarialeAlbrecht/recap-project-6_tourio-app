5a4e1fa69add2b67265ba72359c66cd0c3b9eed6
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-19 16:34:13 +0200

    connected the individual places

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,18 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect.js";
+import Place from "@/db/models/place.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    if (!place) {
+      response.status(404).json({ status: "Not found" });
+      return;
+    }
+    response.status(200).json(place);
   }
 
-  response.status(200).json(place);
+  response.status(405).jason({ status: "Method not allowed" });
 }
