206015034da20915bb867b389b733268508ccca2
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-20 18:31:19 +0200

    feature:create place

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,9 +4,20 @@ import Place from "@/db/models/place";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const places = await Place.find();
-    response.status(200).json(places);
+  try {
+    if (request.method === "GET") {
+      const places = await Place.find();
+      response.status(200).json(places);
+      return;
+    }
+    if (request.method === "POST") {
+      const newPlace = request.body;
+      await Place.create(newPlace);
+      response.status(201).json({ status: "new place created" });
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "Internal server error" });
     return;
   }
   response.status(405).json({ status: "Method not allowed." });
